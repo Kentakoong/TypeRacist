2054ff7df51d1b2e5bafe143f0e72c77aea20970
Wongkraiwich Chuenchomphu <wongkraiwich@inedible.dev>
2025-02-17 18:44:14 +0700

    feat: rebase feat/start-pane into feat/core-logic

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,6 @@
 module dev.typeracist.typeracist {
     requires javafx.controls;
     requires org.json;
-    requires javafx.fxml;
 
     exports dev.typeracist.typeracist.application;
-    opens dev.typeracist.typeracist to javafx.fxml;
-    exports dev.typeracist.typeracist;
 }
