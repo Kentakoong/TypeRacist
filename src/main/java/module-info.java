c2958a7f52bb804728adb9299a3863fe5628ba2b
Wongkraiwich Chuenchomphu <wongkraiwich@inedible.dev>
2025-02-17 18:44:14 +0700

    feat: update-MapScene

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,10 @@
 module dev.typeracist.typeracist {
     requires javafx.controls;
     requires org.json;
+    requires javafx.fxml;
 
     exports dev.typeracist.typeracist.application;
+
+    opens dev.typeracist.typeracist to javafx.fxml;
+    exports dev.typeracist.typeracist;
 }
