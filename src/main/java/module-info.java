aacd6aad6322e29e4741240acc14a862dd85acdf
Okumans <kaopancraft@gmail.com>
2025-02-18 01:11:43 +0700

    feat: add test scenes.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,8 +2,5 @@ module dev.typeracist.typeracist {
     requires javafx.controls;
     requires javafx.fxml;
 
-
-    opens dev.typeracist.typeracist to javafx.fxml;
     exports dev.typeracist.typeracist.application;
-    opens dev.typeracist.typeracist.application to javafx.fxml;
 }
