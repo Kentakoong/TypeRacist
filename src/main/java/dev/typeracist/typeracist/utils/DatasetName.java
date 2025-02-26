9747cf018c1176a0c46451d012fd5447a12a0b2e
Okumans <kaopancraft@gmail.com>
2025-02-26 12:09:23 +0700

    feat: renamed all instance of DataSet to Dataset

--- src/main/java/dev/typeracist/typeracist/utils/DatasetName.java
+++ src/main/java/dev/typeracist/typeracist/utils/DatasetName.java
@@ -1,6 +1,6 @@
 package dev.typeracist.typeracist.utils;
 
-public class DataSetName {
+public class DatasetName {
     final public static String COMMON_WORDS_1K = "common-words-1k";
     final public static String COMMON_WORDS_25K = "common-words-1k";
     final public static String POPULAR_BOOKS = "popular-books";
