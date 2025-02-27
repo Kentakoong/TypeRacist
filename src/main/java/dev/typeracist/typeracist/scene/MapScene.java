d1450d3a704cfb43237ba30c3ab36596f933b391
Ackaraphon Sahawiriya <ackaraphonackaraphon@gmail.com>
2025-02-27 18:40:03 +0700

    feat: add more level and adjust position

--- src/main/java/dev/typeracist/typeracist/scene/MapScene.java
+++ src/main/java/dev/typeracist/typeracist/scene/MapScene.java
@@ -23,18 +23,39 @@ public class MapScene extends Scene {
 
         //  System.out.println(this.getClass().getResource("/dev/typeracist/typeracist/image/anvil.jpg").toString());
 
-        createNode("sword1", 100, 300, this.getClass().getResource("/dev/typeracist/typeracist/image/map/sword.png").toString(), "BATTLE");
-        createNode("chest1", 400, 100, this.getClass().getResource("/dev/typeracist/typeracist/image/map/chest.png").toString(), "REWARD");
-        createNode("shop1", 100, 400, this.getClass().getResource("/dev/typeracist/typeracist/image/map/shop.png").toString(), "STORE");
-        createNode("anvil1", 250, 250, this.getClass().getResource("/dev/typeracist/typeracist/image/map/anvil.png").toString(), "UPGRADE");
-        createNode("skull", 400, 350, this.getClass().getResource("/dev/typeracist/typeracist/image/map/skull.png").toString(), "BOSS");
-        createNode("next", 500, 400,this.getClass().getResource("/dev/typeracist/typeracist/image/map/next.png").toString(), "NEXT");
+        createNode("castle", 75, 490, this.getClass().getResource("/dev/typeracist/typeracist/image/map/castle.png").toString(), "START");
+        createNode("shop", 200, 520, this.getClass().getResource("/dev/typeracist/typeracist/image/map/shop.png").toString(), "STORE");
+        createNode("chest1", 175, 60, this.getClass().getResource("/dev/typeracist/typeracist/image/map/chest.png").toString(), "REWARD1");
+        createNode("chest2", 675, 180, this.getClass().getResource("/dev/typeracist/typeracist/image/map/chest.png").toString(), "REWARD2");
+        createNode("anvil1", 275, 60, this.getClass().getResource("/dev/typeracist/typeracist/image/map/anvil.png").toString(), "UPGRADE");
+        createNode("sword1", 135, 375, this.getClass().getResource("/dev/typeracist/typeracist/image/map/sword.png").toString(), "BATTLE1");
+        createNode("sword2", 200, 250, this.getClass().getResource("/dev/typeracist/typeracist/image/map/sword.png").toString(), "BATTLE2");
+        createNode("sword3", 250,400 , this.getClass().getResource("/dev/typeracist/typeracist/image/map/sword.png").toString(), "BATTLE3");
+        createNode("sword4", 75, 150, this.getClass().getResource("/dev/typeracist/typeracist/image/map/sword.png").toString(), "BATTLE4");
+        createNode("sword5", 350, 150, this.getClass().getResource("/dev/typeracist/typeracist/image/map/sword.png").toString(), "BATTLE5");
+        createNode("sword6", 500, 125, this.getClass().getResource("/dev/typeracist/typeracist/image/map/sword.png").toString(), "BATTLE6");
+        createNode("sword7", 585, 60, this.getClass().getResource("/dev/typeracist/typeracist/image/map/sword.png").toString(), "BATTLE7");
+        createNode("sword8", 700, 50, this.getClass().getResource("/dev/typeracist/typeracist/image/map/sword.png").toString(), "BATTLE8");
+        createNode("sword9", 600, 250, this.getClass().getResource("/dev/typeracist/typeracist/image/map/sword.png").toString(), "BATTLE9");
+        createNode("skull", 525, 390, this.getClass().getResource("/dev/typeracist/typeracist/image/map/skull.png").toString(), "BOSS");
+        createNode("next", 700, 450,this.getClass().getResource("/dev/typeracist/typeracist/image/map/next.png").toString(), "NEXT");
 
         // Connect nodes visually
-        connectNodes("sword1", "chest1");
-        connectNodes("sword1", "shop1");
-        connectNodes("sword1", "anvil1");
-        connectNodes("anvil1", "skull");
+        connectNodes("castle", "shop");
+        connectNodes("castle", "sword1");
+        connectNodes("sword4", "chest1");
+        connectNodes("sword9", "chest2");
+        connectNodes("sword5", "anvil1");
+        connectNodes("sword1", "sword3");
+        connectNodes("sword1", "sword2");
+        connectNodes("sword2", "sword4");
+        connectNodes("sword2", "sword5");
+        connectNodes("sword2", "sword5");
+        connectNodes("sword5", "sword6");
+        connectNodes("sword6", "sword7");
+        connectNodes("sword7", "sword8");
+        connectNodes("sword6", "sword9");
+        connectNodes("skull", "sword9");
         connectNodes("skull", "next");
     }
 
@@ -69,11 +90,38 @@ public class MapScene extends Scene {
 
     private void navigate(String action) {
         switch (action) {
-            case "BATTLE":
-                GameLogic.getInstance().getSceneManager().setScene("BATTLE_SCENE");
+            case "BATTLE1":
+                GameLogic.getInstance().getSceneManager().setScene("BATTLE_SCENE1");
                 break;
-            case "REWARD":
-                GameLogic.getInstance().getSceneManager().setScene("REWARD_SCENE");
+            case "BATTLE2":
+                GameLogic.getInstance().getSceneManager().setScene("BATTLE_SCENE2");
+                break;
+            case "BATTLE3":
+                GameLogic.getInstance().getSceneManager().setScene("BATTLE_SCENE3");
+                break;
+            case "BATTLE4":
+                GameLogic.getInstance().getSceneManager().setScene("BATTLE_SCENE4");
+                break;
+            case "BATTLE5":
+                GameLogic.getInstance().getSceneManager().setScene("BATTLE_SCENE5");
+                break;
+            case "BATTLE6":
+                GameLogic.getInstance().getSceneManager().setScene("BATTLE_SCENE6");
+                break;
+            case "BATTLE7":
+                GameLogic.getInstance().getSceneManager().setScene("BATTLE_SCENE7");
+                break;
+            case "BATTLE8":
+                GameLogic.getInstance().getSceneManager().setScene("BATTLE_SCENE8");
+                break;
+            case "BATTLE9":
+                GameLogic.getInstance().getSceneManager().setScene("BATTLE_SCENE9");
+                break;
+            case "REWARD1":
+                GameLogic.getInstance().getSceneManager().setScene("REWARD_SCENE1");
+                break;
+            case "REWARD2":
+                GameLogic.getInstance().getSceneManager().setScene("REWARD_SCENE2");
                 break;
             case "STORE":
                 GameLogic.getInstance().getSceneManager().setScene("STORE_SCENE");
