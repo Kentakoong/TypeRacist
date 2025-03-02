a2bc3068459abb9b2976c0c55d69ca313ed48c6d
Okumans <kaopancraft@gmail.com>
2025-03-02 17:38:06 +0700

    refactor: make BattleScene inherit from BaseScene and renamed wrongly rebase method name

--- src/main/java/dev/typeracist/typeracist/scene/BattleScene.java
+++ src/main/java/dev/typeracist/typeracist/scene/BattleScene.java
@@ -1,7 +1,6 @@
 package dev.typeracist.typeracist.scene;
 
 import dev.typeracist.typeracist.gui.gameScene.BattlePane;
-import dev.typeracist.typeracist.logic.global.BaseDynamicScene;
 
 public class BattleScene extends BaseDynamicScene<BattlePane> {
     public BattleScene(double width, double height) {
@@ -9,5 +8,15 @@ public class BattleScene extends BaseDynamicScene<BattlePane> {
 
         addPane(0, new BattlePane());
         loadPane(0);
+    }
+
+    @Override
+    public void onSceneEnter() {
+
+    }
+
+    @Override
+    public void onSceneLeave() {
+
     }
 }
