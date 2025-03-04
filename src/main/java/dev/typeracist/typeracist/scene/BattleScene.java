3581d5451710b342eddf3f8d8fe7e80819c88e04
Tanutpong Kengkarnpanich <nut20nut@gmail.com>
2025-03-04 17:20:48 +0700

    fix: Enemy constructor

--- src/main/java/dev/typeracist/typeracist/scene/BattleScene.java
+++ src/main/java/dev/typeracist/typeracist/scene/BattleScene.java
@@ -3,12 +3,14 @@ package dev.typeracist.typeracist.scene;
 import dev.typeracist.typeracist.gui.gameScene.BattlePane.BattlePane;
 import dev.typeracist.typeracist.logic.characters.Enemy;
 import dev.typeracist.typeracist.logic.characters.HP;
+import dev.typeracist.typeracist.logic.characters.skills.Evasion;
 import dev.typeracist.typeracist.logic.gameScene.BattlePaneState;
 import dev.typeracist.typeracist.logic.gameScene.BattlePaneStateContext;
 import dev.typeracist.typeracist.logic.gameScene.BattlePaneStateManager;
 import dev.typeracist.typeracist.logic.global.GameLogic;
 import dev.typeracist.typeracist.logic.global.ResourceManager;
 import dev.typeracist.typeracist.utils.DatasetName;
+import dev.typeracist.typeracist.utils.RandomRange;
 import dev.typeracist.typeracist.utils.ResourceName;
 
 public class BattleScene extends BaseDynamicScene<BattlePane> {
@@ -21,8 +23,10 @@ public class BattleScene extends BaseDynamicScene<BattlePane> {
                                         30,
                                         15,
                                         ResourceManager.getImage(ResourceName.IMAGE_CHARACTER_ASSASSIN),
-                                        "Beware, An evil assassin have appear!.",
-                                        null),
+                                        new String[]{"Beware, An evil assassin have appear!."},
+                                        new Evasion(),
+                                        new RandomRange(10, 20),
+                                        new RandomRange(10, 20)),
                                 GameLogic.getInstance().getDatasetManager().getDataSet(DatasetName.COMMON_WORDS_1K))),
                 width,
                 height);
@@ -36,9 +40,7 @@ public class BattleScene extends BaseDynamicScene<BattlePane> {
         sceneStateManager.transitionToState(BattlePaneState.ENEMY_DESCRIPTION);
 
         battlePane.setupEventHandlers(sceneStateManager);
-        battlePane.getAttackButton().setOnMouseClicked(event -> {
-            sceneStateManager.transitionToState(BattlePaneState.PLAYER_ATTACK);
-        });
+        battlePane.getAttackButton().setOnMouseClicked(event -> sceneStateManager.transitionToState(BattlePaneState.PLAYER_ATTACK));
     }
 
     @Override
