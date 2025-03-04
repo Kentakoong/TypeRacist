9007be9462f85bc0f44846bf5f020adaf3753007
Wongkraiwich Chuenchomphu <wongkraiwich@inedible.dev>
2025-03-04 15:32:22 +0700

    refactor: move randomrange to util

--- src/main/java/dev/typeracist/typeracist/logic/characters/enemies/GoblinTypist.java
+++ src/main/java/dev/typeracist/typeracist/logic/characters/enemies/GoblinTypist.java
@@ -2,8 +2,8 @@ package dev.typeracist.typeracist.logic.characters.enemies;
 
 import dev.typeracist.typeracist.logic.characters.Enemy;
 import dev.typeracist.typeracist.logic.characters.HP;
-import dev.typeracist.typeracist.logic.characters.RandomRange;
 import dev.typeracist.typeracist.logic.global.ResourceManager;
+import dev.typeracist.typeracist.utils.RandomRange;
 import dev.typeracist.typeracist.utils.ResourceName;
 
 public class GoblinTypist extends Enemy {
