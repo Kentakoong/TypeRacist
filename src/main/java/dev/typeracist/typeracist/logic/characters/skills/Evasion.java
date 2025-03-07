935da5217795a58509dfa86e437aae394f5a14c8
Okumans <kaopancraft@gmail.com>
2025-03-07 15:26:02 +0700

    fix: bug

--- src/main/java/dev/typeracist/typeracist/logic/characters/skills/Evasion.java
+++ src/main/java/dev/typeracist/typeracist/logic/characters/skills/Evasion.java
@@ -6,20 +6,20 @@ import dev.typeracist.typeracist.logic.characters.SkillOnEnvironment;
 import dev.typeracist.typeracist.logic.game.battle.BattlePaneStateManager;
 
 public class Evasion extends SkillWithProbability implements SkillOnEnvironment {
-    public Evasion() {
-        super("Evasion",
-                "40% chance to dodge the player's attack.",
-                SkillActivationOnState.ACTIVATION_ON_DEFENSE,
-                0.40);
-    }
+  public Evasion() {
+    super("Evasion",
+        "40% chance to dodge the player's attack.",
+        SkillActivationOnState.ACTIVATION_ON_DEFENSE,
+        0.40);
+  }
 
-    @Override
-    public Skill copy() {
-        return new Evasion();
-    }
+  @Override
+  public Skill copy() {
+    return new Evasion();
+  }
 
-    @Override
-    public void useSkill(BattlePaneStateManager manager) {
-        manager.getContext().getCurrentTurnContext().addPlayerAttackModifier(-Integer.MAX_VALUE);
-    }
+  @Override
+  public void useSkill(BattlePaneStateManager manager) {
+    manager.getContext().getCurrentTurnContext().addPlayerAttackModifier(-(int) 1e9);
+  }
 }
